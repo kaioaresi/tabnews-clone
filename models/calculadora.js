4f529664c3808f5602c0d202ad0cb5181c4f9f50
Kaio Cesar <kaioaresi@gmail.com>
2025-02-11 23:07:12 +0000

    feat: add  and clean files

--- models/calculadora.js
+++
@@ -1,9 +0,0 @@
-function somar(numero1, numero2) {
-    if (typeof numero1 !== "number" || typeof numero2 !== "number") {
-        return "Error";
-    }
-
-    return numero1 + numero2;
-}
-
-exports.somar = somar;
