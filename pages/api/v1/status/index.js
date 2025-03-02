ede5cf30c163123c4ff73b3f3ec7ae48d65da812
Kaio Cesar <kaioaresi@gmail.com>
2025-03-02 20:52:38 +0000

    feat: status check db

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database";
 
 async function status(request, response) {
-    const result = await database.query('SELECT 1 + 1 as sum;');
-    console.log(result.rows)
-    response.status(200).json({"status": "status page"});
+    const updateAt = new Date().toISOString();
+    // Version
+    const dbVersionQuery = await database.query('SHOW server_version;');
+    const dbVersionParse = dbVersionQuery.rows[0].server_version;
+    
+    // max_connections
+    const dbMaxConnectionsQuery = await database.query('show max_connections;');
+    const dbMaxConnections = dbMaxConnectionsQuery.rows[0].max_connections;
+
+    // current connections
+    const dbCurrentConnections = await database.query('SELECT count(*) FROM pg_stat_activity;');
+    const dbConnection = dbCurrentConnections.rows[0].count;
+
+    response.status(200).json({
+        update_at: updateAt,
+        db_info: {
+            version: parseFloat(dbVersionParse),
+            max_connections: parseInt(dbMaxConnections),
+            connections: parseInt(dbConnection),
+        },
+    });
 }
- 
+
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("Get to /api/v1/status should return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
     expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+    expect(responseBody.update_at).toBeDefined();
+
+    const parsedUpdateAt = new Date(responseBody.update_at).toISOString(); // validando o retorno é uma data no ISO 8601
+    expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+    // check db version
+    const dbVersion = responseBody.db_info.version;
+    expect(dbVersion).toBe(16);
+
 });
