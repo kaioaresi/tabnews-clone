d1bdc8c0997c769a13f21b8c3d15d3e1cc5f4281
kaio.santos <kaioaresi@gmail.com>
2025-04-19 11:34:15 -0300

    feat: optimize test migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,32 +3,33 @@ import { join } from "node:path"
 
 
 export default async function migrations(request, response) {
+    const defaultMigrationOptions = {
+        databaseUrl: process.env.DATABASE_URL,
+        dir: join("infra","migrations"),
+        dryRun: true,
+        direction: "up",
+        verbose: true,
+        migrationsTable: "pgmigrations",
+    };
+
     if (request.method === "GET") {
-        console.log("Method GET");
-        const migrations = await migrationsRunner({
-            databaseUrl: process.env.DATABASE_URL,
-            dir: join("infra","migrations"),
-            dryRun: true,
-            direction: "up",
-            verbose: true,
-            migrationsTable: "pgmigrations",
-        });
-        response.status(200).json(migrations);
+        const pendingMigrations = await migrationsRunner(defaultMigrationOptions);
+        return response.status(200).json(pendingMigrations);
     }
 
     if (request.method === "POST") {
-        console.log("Method POST");
-        const migrations = await migrationsRunner({
-            databaseUrl: process.env.DATABASE_URL,
-            dir: join("infra","migrations"),
-            dryRun: false,
-            direction: "up",
-            verbose: true,
-            migrationsTable: "pgmigrations",
+        const migratedMigrations = await migrationsRunner({
+            ...defaultMigrationOptions,
+            dryRun: false, // to post need to be false, to real run migration
         });
-        response.status(200).json(migrations);
+
+        if (migratedMigrations.length > 0) {
+            return response.status(201).json(migratedMigrations);
+        }
+
+        return response.status(200).json(migratedMigrations);
     }
 
-    return response.status(405)
+    return response.status(405).end();
 
 }
